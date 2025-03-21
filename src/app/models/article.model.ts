42b368a908bf7a8969ab0025d6339d47bb095634
sadjoaldi <alsannealdi@hotmail.com>
2025-03-21 10:18:54 +0100

    ajout tableau articles et affichages, un peu de style à completer

--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -1,9 +1,11 @@
 export interface Article {
+  id: number;
   title: string;
   content: string;
   image: string;
   createdAt: Date;
   isPublished: boolean;
   likeCount: number;
+  isLiked: boolean;
   categoryName: string;
 }
